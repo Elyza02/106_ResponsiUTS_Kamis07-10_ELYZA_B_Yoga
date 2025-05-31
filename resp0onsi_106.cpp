dd9e6a3c3232acb6c1ffffbaa8ee624beb9f5762
Elyza <elyzazairin@gmail.com>
2025-05-31 09:42:29 +0700

    Add files via upload

---
+++ resp0onsi_106.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main (){
+
+	 int nama;
+	 string nim;
+	 string saldo,alamat;
+    
+    cout <<"==============================="<<endl;
+    cout << "nama:" <<endl;
+    cin >> nama;
+    cout<<"nim:"<<endl;
+    cin >> nim;
+    cout<<"saldo:"<<endl;
+    cin >> saldo;
+     cout<<" alamat:";
+    cin >> alamat;
+
+    cout <<" barang:"<<endl;
+     
+   
+	  
+	  return 0;
+   
+}
